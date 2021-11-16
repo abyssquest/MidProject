ea1b5bc9614231bc81bf55ef57ce1f31e7ce8731
abyssquest <abyssquest@gmail.com>
2021-11-17 01:25:31 +0900

    starting point

---
+++ FriendPet/src/main/java/com/spring/biz/vo/MemberVO.java
@@ -0,0 +1,6 @@
+package com.spring.biz.vo;
+
+public class MemberVO extends VO {
+	String id;
+	String password;
+}
---
+++ FriendPet/src/main/java/com/spring/biz/vo/VO.java
@@ -0,0 +1,7 @@
+package com.spring.biz.vo;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class VO {
+	MultipartFile uploadFile;
+}
